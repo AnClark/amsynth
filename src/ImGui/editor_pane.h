f80a8f7694c27005cc9bb0ad67de4a6c7fb2b2ad
AnClark <clarklaw4701@qq.com>
2021-09-03 11:45:02 +0800

    UI: ImGui UI Gen 1
    NOTICE: Still need rebasing with newer improvements.
    
    Here comes the 1st generation ImGui UI. Still developing!
    
    - Added license to newly-added files comparing with upstream
    - CMakeLists: Add entries for ImGui addons
    - vendor/imgui_addon: Introduce new ImGui addons source file
    - editor_pane: Replaced old test UI source codes with my newly-designed
    codes
    - amsynth_vst_imgui: Set window size to 800x540

--- src/ImGui/editor_pane.h
+++ src/ImGui/editor_pane.h
@@ -1,3 +1,24 @@
+/*
+ *  editor_pane.h
+ *
+ *  Copyright (c) 2021 AnClark Liu
+ *
+ *  This file is part of amsynth.
+ *
+ *  amsynth is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  amsynth is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with amsynth.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <imgui/imgui.h>
 #include <imgui/backends/imgui_impl_glfw.h>
 #include <imgui/backends/imgui_impl_opengl2.h>
@@ -9,6 +30,8 @@
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 
+/** ImGui addons */
+#include <imgui_addons/zynlab/imgui_knob.h>
 
 /** Prefer WINAPI on Windows to reduce dependencies */
 #include <atomic>
@@ -67,3 +90,9 @@ private:
     void _getParamProperties(int parameter_index, double *minimum, double *maximum, double *default_value, double *step_size);
     void _getParamValues();
 };
+
+/** Shortcuts */
+
+// Get parameter range before creating widget
+// You must define float variables "lower", "upper", "step_increment" before this macro
+#define fetchParamRange(index)  _getParamProperties(index, &lower, &upper, nullptr, &step_increment)
